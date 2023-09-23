96d235ba4f938757edda722d0b7e0ec61dd40a34
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2023-09-23 11:24:48 -0300

    chore: fundamentos

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -2,6 +2,7 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
-		System.out.println("Teste");
+		System.out.println("Teste1");
+		System.out.println("Teste2");
 	}
 }
