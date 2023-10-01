9fe9740f65d1c2f57502ea519a841e7ed681ddfe
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2023-09-30 22:22:22 -0300

    feat: aula 20 e 21 sobre variaveis e comentarios

---
+++ exercicios/src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class AreaCircunferencia {
+	public static void main(String[] args) {
+		double r = 3.4;
+		final double PI = Math.PI;
+		double area = PI * Math.pow(r, 2);
+		System.out.print("Area: " + area);
+	}
+}
--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,6 +1,15 @@
 package fundamentos;
 
+/**
+ * @author Jonatas Nascimento 
+ * @single 1.0
+ */
+
 public class PrimeiroPrograma {
+	//Comentario
+	/*
+	 * Comentario
+	 */
 	public static void main(String[] args) {
 		System.out.println("Teste1");
 		System.out.println("Teste2");
